8a089c67702900e178c4d33e92d00307724259ee
goshawk <vincenzo.ampolo@gmail.com>
2012-11-14 15:13:12 -0800

    Updated AsySVD

--- modules/algorithms/recsys_matlab_codes/algorithms/collaborative/latentFactors/AsySVD/onLineRecom_AsySVD.m
+++ modules/algorithms/recsys_matlab_codes/algorithms/collaborative/latentFactors/AsySVD/onLineRecom_AsySVD.m
@@ -1,4 +1,4 @@
-function [recomList] = onLineRecom_AsySVD (userProfile, model,param)
+function [recomList] = onLineRecom_ASYSVD (userProfile, model,param)
 %userProfile = vector with ratings of a single user
 %model = model created with createModel function 
 % model:    model.mu --> average rating
@@ -7,24 +7,34 @@ function [recomList] = onLineRecom_AsySVD (userProfile, model,param)
 %           model.q 
 %           model.x
 %           model.y
+%           [model.p]
+%           [model.z]
 %param.postProcessingFunction = handle of post-processing function (e.g., business-rules)
 %param.userToTest
 
     try
         mu=model.mu;
-        bu=model.bu;
+%XXX        bu=model.bu;
         bi=model.bi;
-        bu_precomputed=model.bu_precomputed;
-        bi_precomputed=model.bi_precomputed;
         x=model.x;
         y=model.y;
         q=model.q;
         ls=size(x,1);
-        user=param.userToTest;
+%XXX        user=param.userToTest;
+
+%XXXXXXXXXXXXXXXXXXXXXXXXXXXXX ->>>>
     catch e
         display e
         error ('missing some model field');
-    end        
+    end
+    try
+        bu=model.bu(param.userToTest);
+    catch e
+        bu=0;
+    end
+%XXXXXXXXXXXXXXXXXXXXXXXXXXXXX <<<<-
+
+       
     pu=zeros(ls,1);
     ratedItems = find(userProfile);
     numRatedItems = length(ratedItems);
@@ -33,7 +43,10 @@ function [recomList] = onLineRecom_AsySVD (userProfile, model,param)
     end        
     for i=1:numRatedItems
         item=ratedItems(i);
-        pu = pu +  (userProfile(item) - (mu+bu_precomputed(user)+bi_precomputed(item)))*x(:,item);
+%XXX        pu = pu +  (userProfile(item) - (mu+bu(user)+bi(item)))*x(:,item);
+%XXXXXXXXXXXXXXXXXXXXXXXXXXXXX ->>>>
+        pu = pu +  (userProfile(item) - (mu+bu+bi(item)))*x(:,item);
+%XXXXXXXXXXXXXXXXXXXXXXXXXXXXX <<<<-
         pu = pu +  y(:,item);
     end
     pu = pu / sqrt(numRatedItems);   
@@ -42,11 +55,11 @@ function [recomList] = onLineRecom_AsySVD (userProfile, model,param)
         
 
     
-    %if (nargin>=3)
-    if(isfield(param,'postProcessingFunction'))
-        if(strcmp(class(param.postProcessingFunction),'function_handle'))
-            recomList=feval(param.postProcessingFunction,recomList,param);
+    if (nargin>=3)
+        if(isfield(param,'postProcessingFunction'))
+            if(strcmp(class(param.postProcessingFunction),'function_handle'))
+                recomList=feval(param.postProcessingFunction,recomList,param);
+            end
         end
     end
-    
 end
