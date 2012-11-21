24a25b3fd08a8bce6bdec5104b0947304bbee594
goshawk <vincenzo.ampolo@gmail.com>
2012-11-21 21:45:10 +0100

    Quick fix for orientation

--- controllers/movie.py
+++ controllers/movie.py
@@ -4,5 +4,6 @@ def show():
     comments = db(db.comments.movie==movie).select()
     cast = movie.persons_in_movies(db.persons_in_movies.role.belongs(db.roles.name=='actor')).select()
     directors = movie.persons_in_movies(db.persons_in_movies.role.belongs(db.roles.name=='director')).select()
-    session.orientation[movie.title] = URL(r=request, args=request.args, vars=request.vars)
+    if session.orientation:
+        session.orientation[movie.title] = URL(r=request, args=request.args, vars=request.vars)
     return dict(movie=movie, comments=comments, cast=cast, directors=directors)
