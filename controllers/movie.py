4cde63ffc61c98059d7f7b50a9efa75ed58452aa
goshawk <vincenzo.ampolo@gmail.com>
2012-11-21 08:25:17 +0100

    Trying to figure out what's going on

--- controllers/movie.py
+++ controllers/movie.py
@@ -1,5 +1,5 @@
 def show():
-	movie_id = request.args(0)
+	movie_id = request.args[0]
 	movie = db.movies[movie_id]
 	comments = db(db.comments.movie==movie).select()
 	cast = movie.persons_in_movies(db.persons_in_movies.role.belongs(db.roles.name=='actor')).select()
