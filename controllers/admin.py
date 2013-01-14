e21a7b07c5ac3ea7fbd1aac71f7a8a05f19646e1
bottluca <ing.bottiluca@gmail.com>
2013-01-14 11:03:32 +0100

    Update controllers/admin.py

--- controllers/admin.py
+++ controllers/admin.py
@@ -35,7 +35,7 @@ def upload():
     return dict(upload=upload)
 
 def upload_form():
-    form = SQLFORM.factory(db.uplds, Field('Creating model function', 'text'), formstyle='divs', _action=URL('admin', 'upload_form'))
+    form = SQLFORM.factory(db.uplds, Field('users_or_emails', 'text'), formstyle='divs', _action=URL('admin', 'upload_form'))
 
     return dict(form=form)
     
