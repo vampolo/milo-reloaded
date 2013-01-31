d44044cf4bec40f9eb17d4274b359b823fa0eba2
bottluca <ing.bottiluca@gmail.com>
2013-01-31 10:58:36 +0100

    Update controllers/admin.py

--- controllers/admin.py
+++ controllers/admin.py
@@ -36,14 +36,15 @@ def upload():
 
 def upload_form():
     form = SQLFORM.factory(db.uplds, formstyle='divs', _action=URL('admin', 'upload_form'))
-    if form.process().accepted:
+    ##if form.process().accepted:
         ##upload_id = db.uplds.insert(**db.uplds._filter_fields(form.vars))
-        response.flash='form accepted'
+        ##response.flash='form accepted'
         ##redirect(URL('index'))
-    elif form.errors:
+    if form.errors:
         response.flash="errors"
     else:
-        response.flash='fill out the form'
+        response.flash='form accepted'
+        redirect(URL('index'))
     return dict(form=form)
     
 def rules_en():
