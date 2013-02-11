144f37c73d8e76b9590d81d1facab44b00e16d64
bottluca <ing.bottiluca@gmail.com>
2013-02-11 11:57:11 +0100

    Update controllers/admin.py

--- controllers/admin.py
+++ controllers/admin.py
@@ -41,7 +41,9 @@ def upload_form():
         #change filenames
         request.vars.model_creator_function.filename = "createModel_" + form.vars.algorithm_identifier_name + ".mat"
         request.vars.recommender_function.filename = "onLineRecom_" + form.vars.algorithm_identifier_name + ".mat"
-        print request.vars.recommender_function.filename
+        print "Uploaded new algorithm: " + form.vars.algorithm_identifier_name
+        print 'Model function: ' + request.vars.model_creator_function.filename
+        print 'Recommender function: ' + request.vars.recommender_function.filename
         upload_id = db.uplds.insert(**db.uplds._filter_fields(form.vars))
         #change direcotry due to alg_type
         response.flash='form accepted'
