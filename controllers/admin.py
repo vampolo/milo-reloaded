b90be8e396f36695d4de232fcc661a0d90ff16c9
bottluca <ing.bottiluca@gmail.com>
2013-02-18 18:13:25 +0100

    Update controllers/admin.py

--- controllers/admin.py
+++ controllers/admin.py
@@ -39,7 +39,7 @@ def upload():
 def upload_form():
     
     #query benchmark
-    van = {{db.auth_user.username}}
+    van = auth.login_bare(username,password)
     print van
     
     form = SQLFORM.factory(db.uplds, formstyle='divs', _action=URL('admin', 'upload_form'))
