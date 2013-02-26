9281b23242d85917ea3eaef050d29f65074af0a6
bottluca <ing.bottiluca@gmail.com>
2013-02-26 08:50:59 +0100

    Update models/menu.py

--- models/menu.py
+++ models/menu.py
@@ -23,8 +23,8 @@ response.google_analytics_id = None
 
 response.menu = [
     (T('Home'), False, URL('default','index'), []),]
-if (auth.has_membership('admin') or auth.has_membership('researcher')):
-#if (auth.has_membership('researcher')):
+#if (auth.has_membership('admin') or auth.has_membership('researcher')):
+if (auth.has_membership('researcher')):
     response.menu += [(T('Researcher Dashboard'), False, URL('admin', 'index'), [])]
 if (auth.has_membership('admin')):
     response.menu += [(T('Admin Dashboard'), False, URL('admin', 'indexplus'), [])]
