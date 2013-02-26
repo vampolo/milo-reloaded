44bbda7e1967939b99a8fbc9232232021aff57b9
bottluca <ing.bottiluca@gmail.com>
2013-02-26 08:58:23 +0100

    Update controllers/admin.py

--- controllers/admin.py
+++ controllers/admin.py
@@ -4,7 +4,7 @@ import csv
 import os
 import shutil
 
-@auth.requires_membership('researcher')
+@auth.requires_membership(research_group)
 def index():    
     
     algorithms = matlab_wrapper.Whisperer.get_algnames()
