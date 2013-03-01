2694f1d85c441b31627e87864d2195e425b41d99
bottluca <ing.bottiluca@gmail.com>
2013-03-01 08:56:28 +0100

    Update admin.py

--- controllers/admin.py
+++ controllers/admin.py
@@ -126,15 +126,11 @@ def promo():
 def ok():
     whois=request.args(0)
     
-    print 'step1'
     #grant privileges
     auth.add_membership('rsc',whois)
     
-    print 'step2'
     #delete pending
     db(db.pending.uid==whois).delete()
-    
-    print 'step3'
     
     return '<p class="alert congrats"><span class="txt"><span class="icon"></span>Request successfully accepted!</span></p>'
 
