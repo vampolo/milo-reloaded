60990328f7816187e71f3721e712a7b790bee542
bottluca <ing.bottiluca@gmail.com>
2013-03-11 12:39:55 +0100

    Update admin.py

--- controllers/admin.py
+++ controllers/admin.py
@@ -95,8 +95,7 @@ def upload_form():
         #setting ownership table
         iid = str(db(db.uplds.algorithm_name==str(form.vars.algorithm_name)).select(db.uplds.id))
         iiid = int(''.join(c for c in iid if c.isdigit()))
-        db.owns.insert(upload=iiid, author=10)
-        #db.owns.insert(upload=iiid, author=int(auth.user_id))
+        db.owns.insert(upload=iiid, author=int(auth.user_id))
 
         #check tables
         #enlist = db(db.uplds).select()
@@ -131,9 +130,8 @@ def myalg():
 		if (int(checkid) == int(auth.user_id)):
 			buff.append(int(algid))
 
-    print 'ciao'
     #select personal uploads
-    myalg = db(db.uplds.id).select()
+    myalg = db(db.uplds.id == buff[]).select()
     print myalg
     
     return dict(myalg=myalg)
