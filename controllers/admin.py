1ee6775068219984efdcbac5583021918b21bf83
bottluca <ing.bottiluca@gmail.com>
2013-03-12 09:11:58 +0100

    Update admin.py

--- controllers/admin.py
+++ controllers/admin.py
@@ -208,10 +208,8 @@ def passage():
         response.flash='record inserted'
         redirect(URL('index'))
     elif form.errors:
-    	print 'errors'
         response.flash="errors"
     else:
-    	print 'fill'
         response.flash='fill out the form'
     return dict(form=form)
     
