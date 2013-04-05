e307616edb4c36ad566ee8e0b402e2a568fecc3f
bottluca <ing.bottiluca@gmail.com>
2013-04-05 17:17:29 +0300

    Update admin.py

--- controllers/admin.py
+++ controllers/admin.py
@@ -188,6 +188,7 @@ def upd_mc():
     algo = alg.split(',')  
     
     #form
+    form.vars.newupd = 'nuovo'
     
     if (algo[4] == 'public'):
     	fname = 'applications/milo/modules/algorithms/recsys_matlab_codes/algorithms/public/' + "createModel_" + algo[1] + ".m"
@@ -195,7 +196,11 @@ def upd_mc():
     	fname = 'applications/milo/modules/algorithms/recsys_matlab_codes/algorithms/private/' + "createModel_" + algo[1] + ".m"
     
     #delete old mc
+    os.remove(fname)
+    
     #insert new mc
+    rnm = 'applications/milo/modules/algorithms/recsys_matlab_codes/algorithms/' + str(form.vars.newupd)
+    os.rename(rnm,fname)
     
     print '\nupdating: ' + fname
     
@@ -211,6 +216,7 @@ def upd_or():
     algo = alg.split(',')  
 
     #form
+    form.vars.newupd = 'nuovo'
     
     if (algo[4] == 'public'):
     	fname = 'applications/milo/modules/algorithms/recsys_matlab_codes/algorithms/public/' + "onLineRecom_" + algo[1] + ".m"
@@ -218,7 +224,11 @@ def upd_or():
     	fname = 'applications/milo/modules/algorithms/recsys_matlab_codes/algorithms/private/' + "onLineRecom_" + algo[1] + ".m"
 
     #delete old or
+    os.remove(fname)
+    
     #insert new or
+    rnm = 'applications/milo/modules/algorithms/recsys_matlab_codes/algorithms/' + str(form.vars.newupd)
+    os.rename(rnm,fname)
     
     print '\nupdating: ' + fname
     
