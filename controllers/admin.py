1a8063b63b01b8cadf880224789b16335263edf4
bottluca <ing.bottiluca@gmail.com>
2013-04-08 14:03:27 +0300

    Update admin.py

--- controllers/admin.py
+++ controllers/admin.py
@@ -545,10 +545,10 @@ def surveys():
     return dict(surveys=surveys)
 
 def create_survey():
-    form = SQLFORM.factory(db.surveys, Field('users_or_emails', 'text'), formstyle='divs', _action=URL('admin', 'create_survey'))
+    form = SQLFORM.factory(db.surveys, Field('user_emails', 'text'), formstyle='divs', _action=URL('admin', 'create_survey'))
     if form.process().accepted:
         survey_id = db.surveys.insert(**db.surveys._filter_fields(form.vars))
-        for email in form.vars.users_or_emails.split(','):
+        for email in form.vars.user_emails.split(','):
             email = email.strip()
             if len(email) == 0:
                 continue
