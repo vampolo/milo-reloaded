5081163288159032b251ac7816df0af4d5d32d85
bottluca <ing.bottiluca@gmail.com>
2013-04-13 12:40:04 +0300

    Update admin.py

--- controllers/admin.py
+++ controllers/admin.py
@@ -155,9 +155,10 @@ def download_or():
     
     print '\ndownloading: ' + fname
     
+    filename = 'applications/milo/modules/algorithms/recsys_matlab_codes/algorithms/public/try.m'
     #download function
-    response.headers['Content-Disposition'] = 'attachment; filename={}.m'.format(fname)
-    return response.stream(fname)
+    response.headers['Content-Disposition'] = 'attachment; filename={}.m'.format(filename)
+    return response.stream(filename)
     
     #return '<p class="alert congrats"><span class="txt"><span class="icon"></span>Operation was successful!</span></p>'
 
