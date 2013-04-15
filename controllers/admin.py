a564044ac7111924d1e3d54366e49876fbd716fa
bottluca <ing.bottiluca@gmail.com>
2013-04-15 18:19:44 +0300

    Update admin.py

--- controllers/admin.py
+++ controllers/admin.py
@@ -54,18 +54,14 @@ def update_algorithm():
 def upload_form():
     
     #check tables
-    enlist = db(db.uplds).select()
-    print enlist
-    owners = db(db.owns).select()
-    print owners
+    #enlist = db(db.uplds).select()
+    #print enlist
+    #owners = db(db.owns).select()
+    #print owners
     
     #reset tables
     #db.uplds.truncate()
     #db.owns.truncate()
-    
-    dst2 = 'applications/milo/modules/algorithms/recsys_matlab_codes/algorithms/public/'
-    shutil.move(rnm1b, dst2)
-
     
     form = SQLFORM.factory(db.uplds, formstyle='divs', _action=URL('admin', 'upload_form'))
             
